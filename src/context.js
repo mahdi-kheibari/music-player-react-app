a49ea5ccb4e8fc989ad765ac5dcdfd5a9d31d422
Mahdi Kheibari <mahdi.khaybari@gmail.com>
2021-06-20 16:13:14 +0430

    added context api (for state management)

---
+++ src/context.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import AllSongsList from './components/AllSongsList'
+export const MyContext=createContext({currentSong:""});
+
+const Context = ({children}) => {
+    const [currentSong, setCurrentSong] = useState([{
+        name:AllSongsList()[0].name,
+        singer:AllSongsList()[0].singer,
+        id:AllSongsList()[0].id,
+        cover:AllSongsList()[0].cover,
+        address:AllSongsList()[0].address
+    }])
+    return ( 
+    <MyContext.Provider value={{currentSong}}>
+        {children}
+    </MyContext.Provider>
+     );
+}
+ 
+export default Context;
