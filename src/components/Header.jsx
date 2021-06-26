c513e9e907a0b3152b0479d76d7f03a9bc364a22
Mahdi Kheibari <mahdi.khaybari@gmail.com>
2021-06-27 01:00:31 +0430

    Fixed any styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
-import { Button, InputGroup } from 'react-bootstrap';
-import { Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { RiNeteaseCloudMusicLine } from 'react-icons/ri';
-import { FaSearch } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
@@ -13,14 +11,6 @@ const Header = () => {
                     <NavLink to="/" exact activeClassName="active" className="nav-link">All songs</NavLink>
                     <NavLink to="/fav" className="nav-link">Favorite songs</NavLink>
                 </Nav>
-                <Form inline>
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                        <Button variant="outline-light" className="btn_search"><FaSearch/></Button>
-                        </InputGroup.Prepend>
-                        <FormControl type="text" placeholder="Search"/>
-                    </InputGroup>
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
